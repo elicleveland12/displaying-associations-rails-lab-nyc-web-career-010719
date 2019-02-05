11ebded325aaf07a1a6a4f55f577316a4423b0cb
Elijah Cleveland <elicleveland12@gmail.com>
2019-02-05 18:01:06 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,18 +1,21 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
+    @song = Song.new
   end
 
   def create
-    @song = Song.new(song_params)
-
-    if @song.save
-      redirect_to @song
+    @song = Song.new(song_params(:title, :artist_id))
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
     else
       render :new
     end
@@ -24,14 +27,13 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
-    end
+  @song.update(song_params(:title, :artist_id))
+  if @song.valid?
+    @song.save
+    redirect_to song_path(@song)
+  else
+    render :edit
+  end
   end
 
   def destroy
@@ -43,8 +45,7 @@ class SongsController < ApplicationController
 
   private
 
-  def song_params
-    params.require(:song).permit(:title)
+  def song_params(*args)
+    params.require(:song).permit(*args)
   end
 end
-
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,13 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  def song_count
+    arr = []
+    Song.all.select do |song|
+      if song.artist_id == self.id
+        arr << song.title
+      end
+    end
+    arr.length
+  end
 end
